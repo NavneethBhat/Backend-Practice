0449a2e43cb7dcd31d173cf5c080669254b5de5a
NaveethBhat <navnithbhat007@gmail.com>
2024-12-18 11:21:59 +0530

    11:21 Am

---
+++ src/config/Global.js
@@ -0,0 +1,16 @@
+const RESPONSE = {
+  SUCCESS: {
+    code: 200,
+    message: "Success",
+  },
+  ERROR: {
+    code: 500,
+    message: "Error",
+  },
+  REQUIRED: {
+    code: 500,
+    message: " is Required",
+  },
+};
+
+export default RESPONSE;
--- src/controllers/manageStudents/addStudent.js
+++ src/controllers/manageStudents/addStudent.js
@@ -1,16 +1,47 @@
-import { Router } from "express";
+import { response, Router } from "express";
 const router = Router();
+import studentModel from "../../models/studentModel.js";
+import RESPONSE from "../../config/Global.js";
 
 router.post("/", (req, res) => {
   try {
     const { name, rollno, email } = req.body;
+
+    if (!name || name == undefined) {
+      const response = RESPONSE.REQUIRED;
+      return res.json({
+        code: response.code,
+        message: "name" + response.message,
+      });
+    }
+
+    if (!rollno || rollno == undefined) {
+      const response = RESPONSE.REQUIRED;
+      return res.json({
+        code: response.code,
+        message: "rollno" + response.message,
+      });
+    }
+
+    if (!email || email == undefined) {
+      const response = RESPONSE.REQUIRED;
+      return res.json({
+        code: response.code,
+        message: "email" + response.message,
+      });
+    }
+
     console.log({ name, rollno, email });
 
-    res.json({
-      responseMessage: "All Good",
+    studentModel.create({
+      name: name,
+      rollno: rollno,
+      email: email,
     });
+
+    res.json(RESPONSE.SUCCESS);
   } catch (error) {
-    console.log(error);
+    console.log("Error!");
   }
 });
 
