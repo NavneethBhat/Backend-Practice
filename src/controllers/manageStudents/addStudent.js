8e8de66cff596af9ef1d499f203fa406c4d265b8
NaveethBhat <navnithbhat007@gmail.com>
2024-12-19 10:13:24 +0530

    10:13 Am

--- src/controllers/manageStudents/addStudent.js
+++ src/controllers/manageStudents/addStudent.js
@@ -2,33 +2,39 @@ import { response, Router } from "express";
 const router = Router();
 import studentModel from "../../models/studentModel.js";
 import RESPONSE from "../../config/Global.js";
+import { send, setErrorsRes } from "../../helper/responseHelper.js";
 
 router.post("/", (req, res) => {
   try {
     const { name, rollno, email } = req.body;
 
     if (!name || name == undefined) {
-      const response = RESPONSE.REQUIRED;
-      return res.json({
-        code: response.code,
-        message: "name" + response.message,
-      });
+      // const response = RESPONSE.REQUIRED;
+      // return res.json({
+      //   code: response.code,
+      //   message: "name" + response.message,
+      // });
+      return send(res, setErrorsRes(RESPONSE.REQUIRED, "name"));
     }
 
     if (!rollno || rollno == undefined) {
-      const response = RESPONSE.REQUIRED;
-      return res.json({
-        code: response.code,
-        message: "rollno" + response.message,
-      });
+      // const response = RESPONSE.REQUIRED;
+      // return res.json({
+      //   code: response.code,
+      //   message: "rollno" + response.message,
+      // });
+
+      return send(res, setErrorsRes(RESPONSE.REQUIRED, "rollno"));
     }
 
     if (!email || email == undefined) {
-      const response = RESPONSE.REQUIRED;
-      return res.json({
-        code: response.code,
-        message: "email" + response.message,
-      });
+      // const response = RESPONSE.REQUIRED;
+      // return res.json({
+      //   code: response.code,
+      //   message: "email" + response.message,
+      // });
+
+      return send(res, setErrorsRes(RESPONSE.REQUIRED, "email"));
     }
 
     console.log({ name, rollno, email });
