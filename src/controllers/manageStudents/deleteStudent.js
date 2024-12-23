a6ada5401af83050cf09a3c1b44fa6d1960b44f9
NaveethBhat <navnithbhat007@gmail.com>
2024-12-23 10:50:46 +0530

    10:50 Am

--- src/controllers/manageStudents/deleteStudent.js
+++ src/controllers/manageStudents/deleteStudent.js
@@ -9,6 +9,9 @@ import validator from "validator";
 router.delete("/", async (req, res) => {
   try {
     let student_id = req.query.student_id;
+    if (!student_id || student_id == undefined) {
+      return send(res, setErrorsRes(RESPONSE.REQUIRED, "Student ID"));
+    }
     let studentData = await studentModel.aggregate([
       {
         $match: {
